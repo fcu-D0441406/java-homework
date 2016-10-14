ca07a613afd627522043aeb0b547119947c4d78a
fcu-D0441406 <ian061504@gmail.com>
2016-10-14 09:53:33 +0800

    Update StringComparator.java

--- src/KeyboardScanner/StringComparator.java
+++ src/KeyboardScanner/StringComparator.java
@@ -5,16 +5,25 @@ import java.util.Scanner;
 public class StringComparator {
 
 	public static void main(String[] args) {
+		int flag = 1;
 		String a , b;
 		Scanner abcde = new Scanner(System.in);
 		a = abcde.next();
 		b = abcde.next();
-		if(a.equalsIgnoreCase(b)){
-		    System.out.println("The two strings are the same.");
-		}
-		else
+		if(a.equalsIgnoreCase(b))
 		{
-			System.out.println("The two strings are not the same.");
+			System.out.println("this is same");
+			flag = 0;
+		}
+		while(flag==1)
+		{
+			a = abcde.next();
+			b = abcde.next();
+			if(a.equalsIgnoreCase(b))
+			{
+				System.out.println("this is same");
+				flag = 0;
+			}
 		}
 
 	}
